ada1ba245a284485c4626eca73023c9200a904e7
alexchancey <achancey27@scienceleadership.org>
2026-04-07 14:55:39 -0400

    fixed an issue where it would print 2 too many numbers

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,33 +1,42 @@
+import com.sun.source.tree.SynchronizedTree;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Fibonacci implements FibonacciLike{
+public class Fibonacci implements FibonacciLike, Runnable{
+    private static final Object fibLock = new Object();
     int amountofNumbersInSequence = 0;
     int number1 = 0;
     int number2 = 0;
-
-    public Fibonacci(int number1, int number2, int amountofNumbersInSequence) {
+    String name;
+    long wait;
+    public Fibonacci(String name,long wait,int number1, int number2, int amountofNumbersInSequence) {
         this.number2 = number2;
         this.number1 = number1;
         this.amountofNumbersInSequence = amountofNumbersInSequence;
+        this.name=name;
+        this.wait=wait;
     }
     ArrayList<Integer> numbers;
 
     public void calculate(){
         numbers = new ArrayList<>();
+
         numbers.add(number1);
         numbers.add(number2);
 
-        calculateRecursive(number1,number2,amountofNumbersInSequence-2);
-        /*
-        for(int i=0;i<amountofNumbersInSequence-2;i++){
-        numbers.add(numbers.get(numbers.size()-1)+numbers.get(numbers.size()-2));
+        for (int i = 0; i < amountofNumbersInSequence - 2; i++) {
+            numbers.add(numbers.get(numbers.size() - 1) + numbers.get(numbers.size() - 2));
+
+            System.out.println(name + ": " + numbers);
+
+            try {
+                Thread.sleep(wait);
+            } catch (Exception e) {
+            }
         }
 
-         */
-
-        System.out.println(numbers);
     }
 
 
@@ -40,5 +49,10 @@ public class Fibonacci implements FibonacciLike{
         else{
             return Arrays.asList(num1,num2);
         }
+    }
+
+
+    public void run(){
+    calculate();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 
-void main() {
-    Fibonacci fib1 = new Fibonacci(0,1,5);
-    Fibonacci fib2 = new Fibonacci(0,2,10);
-    fib1.calculate();
-    fib2.calculate();
-    fib2.calculate();
-
+void main() throws Exception {
+    Fibonacci fib1 = new Fibonacci("tim",200,0,1,10);
+    Fibonacci fib2 = new Fibonacci("joe",200,0,5,10);
+    Fibonacci fib3 = new Fibonacci("mike",200,0,3,10);
+    Thread thread1 = new Thread(fib1);
+    Thread thread2 = new Thread(fib2);
+    Thread thread3 = new Thread(fib3);
+    thread1.start();
+    thread2.start();
+    thread3.start();
 }
